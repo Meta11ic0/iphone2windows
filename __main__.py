5a1eeb03b21d26a34a4331b2f60230f899fff41b
Johnny <ihaveabigdoor@gmail.com>
2024-09-24 13:37:20 +0800

    1

--- __main__.py
+++ __main__.py
@@ -4,8 +4,93 @@ import os
 import pathlib
 from datetime import datetime
 
-import win32utils
-from win32utils import CopyParams
+from dataclasses import dataclass
+
+import pythoncom
+from win32comext.shell import shell, shellcon
+
+
+@dataclass
+class CopyParams:
+    file_shell: object
+    destination_folder_shell: object
+    desination_file_name: str
+
+# helper function
+def remove_prefix(str, prefix):
+    if not str.startswith(prefix):
+        raise Exception(f"'{str}' should start with '{prefix}")
+    return str[len(prefix):]
+
+
+def get_desktop_shell():
+    return shell.SHGetDesktopFolder()
+
+
+def get_file_full_path(file_shell):
+    return file_shell.GetDisplayName(shellcon.SIGDN_DESKTOPABSOLUTEEDITING)
+
+
+# 
+def get_child_folder_shell(parent_folder_shell, child_folder_name: str):
+    for child_pidl in parent_folder_shell:
+        folder_name = parent_folder_shell.GetDisplayNameOf(child_pidl, shellcon.SHGDN_NORMAL)
+        if folder_name == child_folder_name:
+            return parent_folder_shell.BindToObject(child_pidl, None, shell.IID_IShellFolder)
+    raise Exception(f"Cannot find {child_folder_name}")
+
+
+# safetly, only for folder
+def get_folder_shell_from_str(folder_str):
+    current_folder_shell = get_desktop_shell()
+    folders = folder_str.split("\\")
+    for folder in folders:
+        try:
+            current_folder_shell = get_child_folder_shell(current_folder_shell, folder)
+        except BaseException as exception:
+            raise Exception(f"Cannot get shell folder for {folder_str} (at '{folder}')") from exception
+    return current_folder_shell
+
+
+# quickly, without check
+def get_shell_from_str(path_str):
+    try:
+        return shell.SHCreateItemFromParsingName(path_str, None, shell.IID_IShellItem)
+    except BaseException as exception:
+        raise Exception(f"Cannot get shell for {path_str}") from exception
+
+
+
+# 
+def get_files_dict_from_shell(folder_shell):
+    result = {}
+    for folder_pidl in folder_shell.EnumObjects(0, shellcon.SHCONTF_FOLDERS):
+        child_folder_shell = folder_shell.BindToObject(folder_pidl, None, shell.IID_IShellFolder)
+        child_folder_name = folder_shell.GetDisplayNameOf(folder_pidl, shellcon.SHGDN_FORADDRESSBAR)
+        print(f"Listing folder '{child_folder_name}'")
+        result |= get_files_dict_from_shell(child_folder_shell)
+    for file_pidl in folder_shell.EnumObjects(0, shellcon.SHCONTF_NONFOLDERS):
+        file_directory_pidl = shell.SHGetIDListFromObject(folder_shell)
+        file_shell = shell.SHCreateShellItem(file_directory_pidl, None, file_pidl)
+        file_full_path_str = get_file_full_path(file_shell)
+        result[file_full_path_str] = file_shell
+    return result
+
+
+def copy_multiple_files(copy_params_list: list[CopyParams]):
+    fileOperationObject = pythoncom.CoCreateInstance(shell.CLSID_FileOperation,
+                                                     None,
+                                                     pythoncom.CLSCTX_ALL,
+                                                     shell.IID_IFileOperation)
+    for copy_params in copy_params_list:
+        src_str = get_file_full_path(copy_params.source_file_shell)
+        dst_str = get_file_full_path(copy_params.destination_folder_shell)
+        print(f"Queuing copying '{src_str}' to '{dst_str}'")
+        fileOperationObject.CopyItem(copy_params.source_file_shell, copy_params.destination_folder_shell,
+                                     copy_params.desination_file_name)
+    print(f"Running copy operations...")
+    fileOperationObject.PerformOperations()
+
 
 # get imported files from record 
 def read_records(record_folder):
@@ -29,33 +114,18 @@ def read_records(record_folder):
 # get all file from folder
 # return a dict key is a file relative path, e.g. "202301_a\IMG_1694.jpg"
 # value is windows shell of this file "IMG_1694.jpg" 
-def get_files_dict(folder_str):
-    source_folder_shell = win32utils.get_folder_shell_from_str(folder_str)
-    files_dict = win32utils.get_files_dict_from_shell(source_folder_shell)
-    return files_dict
-
-
-def remove_prefix(str, prefix):
-    if not str.startswith(prefix):
-        raise Exception(f"'{str}' should start with '{prefix}")
-    return str[len(prefix):]
-
-
-# based on imported files which get from record
-# classify files into 'to import' and 'not import' categories.
-def classify_files(source_folder_str, files_dict, imported_files_set):
-    not_import_files_set = set()
-    files_to_import_dict = {}
-    for path in sorted(files_dict.keys()):
-        file_shell = files_dict[path]
-        full_path_str = win32utils.get_file_full_path(file_shell)
+def get_files_dict(source_folder_str, imported_files_set):
+    files_dict = {}
+    source_folder_shell = get_folder_shell_from_str(source_folder_str)
+    all_files_dict = get_files_dict_from_shell(source_folder_shell)
+    for path in sorted(all_files_dict.keys()):
+        file_shell = all_files_dict[path]
+        full_path_str = get_file_full_path(file_shell)
         relative_path_str = remove_prefix(full_path_str, source_folder_str)
         relative_path_str = remove_prefix(relative_path_str, '\\')
         if relative_path_str not in imported_files_set:
-            files_to_import_dict[relative_path_str] = file_shell
-        else:
-            not_import_files_set.add(relative_path_str)
-    return  not_import_files_set, files_to_import_dict
+            files_dict[relative_path_str] = file_shell
+    return files_dict
 
 
 # using windows shell to import
@@ -69,13 +139,13 @@ def import_files(files_dict, destination_folder_str):
         # if desination file directory not exits, build directory and mark
         if desination_file_directory_str not in destination_directorys_dict:
             pathlib.Path(desination_file_directory_str).mkdir(parents=True, exist_ok=True)
-            destination_folder_shell = win32utils.get_shell_item_from_path(desination_file_directory_str)
+            destination_folder_shell = get_shell_from_str(desination_file_directory_str)
             destination_directorys_dict[desination_file_directory_str] = destination_folder_shell
         file_shell = files_dict[file_relative_path_str]
         copy_params = CopyParams(file_shell, destination_directorys_dict[desination_file_directory_str],
                                  desination_file_name)
         copy_params_list.append(copy_params)
-    win32utils.copy_multiple_files(copy_params_list)
+    copy_multiple_files(copy_params_list)
 
 # record imported files in this process  
 def write_record(record_folder, files_dict):
@@ -89,24 +159,16 @@ def write_record(record_folder, files_dict):
 
 def main(args):
     print(f"Program args: {args.__dict__}")
-
     source_folder_str = args.source
     destination_folder_str = args.destination
-
     imported_files_set = read_records(args.record_folder)
-
-    all_files_dict = get_files_dict(source_folder_str)
-
-    not_import_files_set, files_to_import_dict = classify_files(
-        source_folder_str, all_files_dict, imported_files_set)
-
-    print(f"Import {len(files_to_import_dict)} files")
-
+    files_dict = get_files_dict(source_folder_str, imported_files_set)
+    print(f"Import {len(files_dict)} files")
     if args.skip_copy:
         print(f"skip-copy mode, skipping copying")
-    elif len(files_to_import_dict) > 0:
-        import_files(files_to_import_dict, destination_folder_str)
-        write_record(args.record_folder, files_to_import_dict)
+    elif len(files_dict) > 0:
+        import_files(files_dict, destination_folder_str)
+        write_record(args.record_folder, files_dict)
     else:
         print(f"Nothing to copy")
         
